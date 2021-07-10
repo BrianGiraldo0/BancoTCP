bd29c9cb0a64d662c85255f76d33ad0bd2976cdc
Cesar <cesare.marquezm@uqvirtual.edu.co>
2021-07-09 21:57:36 -0500

    Etapa 3 añadida

--- src/co/edu/uniquindio/cliente/BancoClientProtocol.java
+++ src/co/edu/uniquindio/cliente/BancoClientProtocol.java
@@ -1,33 +1,51 @@
 package co.edu.uniquindio.cliente;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import co.edu.uniquindio.banco.Cuenta;
+import co.edu.uniquindio.resources.Loader;
 import co.edu.uniquindio.server.BancoServer;
 
 public class BancoClientProtocol {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private static PrintWriter toNetwork;
-
+    private static ArrayList<String> lista;
+    private static int cont;
     private static BufferedReader fromNetwork;
-
+    private static Socket socket1;
+    private static boolean permiteRecibir=true;
     public static void protocol (Socket socket) throws Exception {
+    	Thread.sleep(100);
     	String fromUser="";
         createStreams(socket);
         
+        if(permiteRecibir) {
         	 System.out.print("Ingrese una opción: ");
              fromUser = SCANNER.nextLine();
-             
              toNetwork.println(readMenu(fromUser));
-
+        }else {
+        	if(cont<lista.size()) {
+        		System.out.println(cont +", " + lista.get(cont));
+        		toNetwork.println(lista.get(cont));
+        		System.out.println("enviado");
+        		cont++;
+        		if(cont==lista.size()) {
+        			permiteRecibir=true;
+        			cont =0;
+        		}
+        	}
+        }
+        
              String fromServer = fromNetwork.readLine();
              System.out.println("[Client] From server: " + fromServer);
-       
+    	
 
 
     }
@@ -36,8 +54,12 @@ public class BancoClientProtocol {
         fromNetwork = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
     
+    public static void cargarTransacciones(String URL) throws IOException, InterruptedException {
+    	permiteRecibir=false;
+    	lista = Loader.cargarTransacciones(URL);
+    }
     
-    public static String readMenu(String option) {
+    public static String readMenu(String option) throws IOException, InterruptedException {
     	int opt = Integer.parseInt(option);
     	String numCuenta = "";
     	String command ="";
@@ -107,11 +129,21 @@ public class BancoClientProtocol {
 				command = "CONSULTAR " + numCuenta;
 			break;
 		
-		case 9: BancoClient.IteradorInfinito = false;
+		case 9: System.out.print("Ingrese el nombre del archivo: ");
+				String nombre = SCANNER.nextLine();
+				command = "CARGAR " + nombre;
+				cargarTransacciones(nombre);
+				
+			break;
+			
+		case 10: BancoClient.IteradorInfinito = false;
 				command= "SALIR";
 
 		}
     	
     	return command;
     }
+    
+    
+    
 }
